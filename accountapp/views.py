987f7e37b1b83a3f23b96f95c8a8c1e75191637a
jtue0 <j_tue@naver.com>
2021-07-13 12:00:00 +0900

    7/n clsaa4 commit4 (css start)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from accountapp.models import NewModel
+
+
 def go_home(request):
-    return render(request, 'accountapp/go_home.html')
+    if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+        return HttpResponseRedirect(reverse('accountapp:go_home'))
+
+
+    else:
+        data_list = NewModel.objects.all()
+        return render(request, 'accountapp/go_home.html',
+                      context={'data_list': data_list})
