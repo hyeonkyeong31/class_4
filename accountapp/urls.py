8298f749ebb32a84aefd5d7f1c4086498f70de4c
jtue0 <j_tue@naver.com>
2021-07-14 17:41:47 +0900

    7/14 signup/login/out

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import go_home, AccountCreateView
@@ -5,6 +6,10 @@ from accountapp.views import go_home, AccountCreateView
 app_name = 'accountapp'
 urlpatterns = [
     path('go_home/', go_home, name = 'go_home'),
+
+    path('login/',LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name = 'create')
 ]
